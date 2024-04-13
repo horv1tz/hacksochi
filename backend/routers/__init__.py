b72fe40ac2a765dbedcf3ede13a3be81e2e6c892
Leonid Dyachenko <horvitz@dvorfs.ru>
2024-04-13 21:14:08 +0300

    Add

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,5 +1,5 @@
-from .admin import app
-from .users import app
-from .service import app
+# from .admin import app
+# from .users import app
+# from .service import app
 
-__all__ = ['app']
+# __all__ = ['app']
